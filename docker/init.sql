1cb00786a93a3be6319876bd6a117a477cf1b43f
Muhammad Rizki Pratama <rizki.qq05@gmail.com>
2025-08-12 10:03:37 +0700

    Remove all files from repository

--- docker/init.sql
+++
@@ -1,8 +0,0 @@
--- contoh schema awal (opsional)
-CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  email TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL,
-  created_at TIMESTAMPTZ DEFAULT now()
-);
